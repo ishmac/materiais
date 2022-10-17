229fcdeef8d65db2117027b8d39c3cbfeff1435a
KuroKyd <115722808+KuroKyd@users.noreply.github.com>
2022-10-17 10:48:49 -0400

    Create lista for com função.py

---
+++ nivelamento/listas:/lista for com função.py
@@ -0,0 +1,54 @@
+'''
+lista = []
+num = 1
+while num != 0:
+    num = int(input())
+    lista.append(num)
+print(lista)
+
+'''
+'''
+lista1= []
+num2 = int(input())
+num1 = 1
+i = 0
+
+while i < 10:
+    num1 = int(input())
+    lista1.append(num1)
+    i += 1
+if num2 in lista1:
+    print(lista1)
+    print("esta na lista")
+else:
+    print(lista1)
+    print("nao esta na lista")
+    '''
+
+'''
+lista = []
+num = 1
+contador = 0
+while contador < 10:
+    num = int(input())
+    lista.append(num)
+    contador += 1
+lista.sort()
+print(lista[0])
+'''
+'''
+lista = []
+num1 = 1
+contador = 0
+total = 0
+while contador < 10:
+    num1 = int(input())
+    lista.append(num1)
+    contador += 1
+for s in lista:
+    total += 1
+
+print(lista)
+print(total)
+'''
+
